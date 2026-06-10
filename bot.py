eed4fd08b5900a1d25aebb4053d1eb9e823cc576
noonon3041 <ruddls330@gmail.com>
2026-06-10 12:57:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("홀덤 스케줄 봇이 정상 작동 중입니다.")
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
